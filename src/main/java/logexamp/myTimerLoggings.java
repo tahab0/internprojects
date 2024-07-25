0877b7f395e2a681258512189c68b2c7c032a2a7
Taha Bayrak <72819562+tahab0@users.noreply.github.com>
2024-07-26 02:14:55 +0300

    first project commit

---
+++ src/main/java/logexamp/myTimerLoggings.java
@@ -0,0 +1,56 @@
+package logexamp;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class myTimerLoggings {
+    private static final Logger debugLogger = LogManager.getLogger("debugLogger");
+    private static final Logger infoLogger = LogManager.getLogger("infoLogger");
+    private static final Logger errorLogger = LogManager.getLogger("errorLogger");
+    private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    public static void main(String[] args) {
+
+
+        Timer debugTimer = new Timer();
+        Timer infoTimer = new Timer();
+        Timer errorTimer = new Timer();
+
+        debugTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                String currentTime = LocalDateTime.now().format(timeFormatter);
+                debugLogger.debug(currentTime);
+            }
+        }, 0, 1000);
+
+        infoTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                String currentTime = LocalDateTime.now().withSecond(0).format(timeFormatter);
+                infoLogger.info(currentTime);
+            }
+        }, 0, 60000);
+
+        errorTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                String currentTime = LocalDateTime.now().withMinute(0).withSecond(0).format(timeFormatter);
+                errorLogger.error(currentTime);
+            }
+        }, 0, 3600000);
+
+        // Uygulama kapanırken zamanlayıcıları durdur
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            debugTimer.cancel();
+            infoTimer.cancel();
+            errorTimer.cancel();
+        }));
+    }
+}
+
